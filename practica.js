7bef6566b2650be8fd554fb74e9056a0997ac026
GarridoTomas <garrido.tomas@hotmail.com>
2020-10-12 16:33:39 -0300

    agregado de estilos

--- practica.js
+++ practica.js
@@ -4,46 +4,45 @@ var departamento;
 var departamentoIngresado;
 
 function init() {
-  
   piso = document.getElementById("piso");
   departamento = document.getElementById("dpto");
   pisoIngresado = new Array();
 }
 
-function tomarDigito(digito){
-  
-  if(pisoIngresado.length == 0){
-    pisoIngresado.push(digito);;
+function tomarDigito(digito) {
+  if (pisoIngresado.length == 0) {
+    pisoIngresado.push(digito);
     piso.textContent = digito;
-  }
-  else{
-    if(pisoIngresado.length == 1){
-      pisoIngresado.push(digito);;
+  } else {
+    if (pisoIngresado.length == 1) {
+      pisoIngresado.push(digito);
       piso.textContent = getPisoIngresado();
       verificarQueElPisoIngresadoSeaValido();
-    }
-    else{
+    } else {
       departamentoIngresado = digito;
       departamento.textContent = digito;
     }
   }
 }
 
-function verificarQueElPisoIngresadoSeaValido(){
-if(! esValidoElPisoIngresado()){
-  piso.textContent = "El piso ingresado ".concat(getPisoIngresado(), " no es valido");
-}
-}
-
-function esValidoElPisoIngresado(){
-return getPisoIngresado() <= 48; 
-}
-
-function getPisoIngresado(){
-return pisoIngresado[0] * 10 + pisoIngresado[1];
+function verificarQueElPisoIngresadoSeaValido() {
+  if (!esValidoElPisoIngresado()) {
+    piso.textContent = "El piso ingresado ".concat(
+      getPisoIngresado(),
+      " no es valido"
+    );
+  }
 }
 
-function borrar(){
+function esValidoElPisoIngresado() {
+  return getPisoIngresado() <= 48;
+}
+
+function getPisoIngresado() {
+  return pisoIngresado[0] * 10 + pisoIngresado[1];
+}
+
+function borrar() {
   piso.textContent = "";
   departamento.textContent = "";
   pisoIngresado = [];
@@ -51,6 +50,10 @@ function borrar(){
   departamentoIngresado = null;
 }
 
-function llamar(){
-  document.getElementById("quePisoEs").textContent = "Llamando al piso " + getPisoIngresado() + ", dpto " + departamentoIngresado;
+function llamar() {
+  document.getElementById("quePisoEs").textContent =
+    "Llamando al piso " +
+    getPisoIngresado() +
+    ", dpto " +
+    departamentoIngresado;
 }
